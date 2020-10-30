406e7e9314169490ebf1b7418b2748649ef168e0
Nil Quera <nlquera97@gmail.com>
2020-10-30 09:46:09 +0100

    Subscribe to proofs and console.log them

--- src/index.js
+++ src/index.js
@@ -1,37 +1,88 @@
 const ethers = require("ethers");
-const network = require("./contracts/network.json");
-const { addEventToCache } = require("./event-router");
+// const { addEventToCache } = require("./event-router");
+const { stampProof, recycleProof, functionProof, transferProof, dataWipeProof, reuseProof } = require("./events")
 
-const provider = new ethers.providers.JsonRpcProvider(network.development.url);
-const stampProofs = new ethers.Contract(
-    network.development.stampProofs,
-    require("./contracts/StampProofs.json").abi,
-    provider
-);
+const provider = new ethers.providers.JsonRpcProvider();
 
-provider.resetEventsBlock(0);
+// provider.resetEventsBlock(0);
 
-var Events;
-(function (Events) {
-  Events["stampProof"] = "stampProof";
-  Events["functionProof"] = "functionProof";
-  Events["transferProof"] = "transferProof";
-  Events["dataWipeProof"] = "dataWipeProof";
-  Events["reuseProof"] = "reuseProof";
-  Events["recycleProof"] = "recycleProof";
-})(Events || (Events = {}));
-
-addEventToCache(Events.functionProof, {
-    deviceAddress: "123456789",
-    diskUsage: 123
-})
-
-stampProofs.on(Events.stampProof, async (id, hash, timestamp) => {
-    addEventToCache(Events.stampProof, {
-        id: Number(id),
-        hash: hash.slice(2),
-        timestamp: Number(timestamp)
-    });
+provider.on(stampProof.filter, log => {
+  const event = stampProof.iface.parseLog(log).args
+  const data = {
+    id: event.id.toNumber(),
+    hash: event.hash,
+    timestamp: event.timestamp.toNumber()
+  }
+  console.log("stampProof", data)
+  // addEventToCache(stampProof.name, {
+    //     id: Number(id),
+    //     hash: hash.slice(2),
+    //     timestamp: Number(timestamp)
+    // });
 });
 
+provider.on(recycleProof.filter, (log) => {
+  const event = recycleProof.iface.parseLog(log).args;
+  const data = {
+    proofHash: event.proofHash,
+    recyclerAddress: event.recyclerAddress,
+    deviceAddress: event.deviceAddress,
+    date: event.date,
+    gpsLocation: event.gpsLocation
+  }
+  console.log("recycleProof", data);
+  
+  // addEventToCache(stampProof.name, {
+  //     id: Number(id),
+  //     hash: hash.slice(2),
+  //     timestamp: Number(timestamp)
+  // });
+});
 
+provider.on(functionProof.filter, (log) => {
+  const event = functionProof.iface.parseLog(log).args;
+  const data = {
+    proofHash: event.proofHash,
+    ownerAddress: event.ownerAddress,
+    deviceAddress: event.deviceAddress,
+    score: event.score.toNumber(),
+    diskUsage: event.diskUsage.toNumber(),
+    algorithmversion: event.algorithmversion
+  }
+  console.log("functionProof", data)
+});
+
+provider.on(transferProof.filter, (log) => {
+  const event = transferProof.iface.parseLog(log).args;
+  const data = {
+    proofHash: event.proofHash,
+    deviceAddress: event.deviceAddress,
+    supplierAddress: event.supplier,
+    receiverAddress: event.receiver
+  }
+  console.log("transferProof", data)
+});
+
+provider.on(dataWipeProof.filter, (log) => {
+  const event = dataWipeProof.iface.parseLog(log).args;
+  const data = {
+    proofHash: event.proofHash,
+    deviceAddress: event.deviceAddress,
+    erasureType: event.erasureType,
+    date: event.date,
+    erasureResult: event.erasureResult
+  }
+  console.log("dataWipeProof", data);
+});
+
+provider.on(reuseProof.filter, (log) => {
+  const event = reuseProof.iface.parseLog(log).args;
+  const data = {
+    proofHash: event.proofHash,
+    deviceAddress: event.deviceAddress,
+    receiverSegment: event.receiverSegment,
+    idReceipt: event.idReceipt,
+    price: event.price.toNumber()
+  };
+  console.log("reuseProof", data);
+});
