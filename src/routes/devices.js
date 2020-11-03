9a138639956276c4c889e8575d5a51296a219793
Nil Quera <nlquera97@gmail.com>
2020-11-03 10:53:24 +0100

    Add GET device route

--- src/routes/devices.js
+++ src/routes/devices.js
@@ -1,20 +1,67 @@
+const ethers = require("ethers");
 const express = require("express");
 const app = express();
-const sql = require("../db")
+const sql = require("../db");
+const { BadRequest, NotFound } = require("../utils/errors");
 
 app.get("/:deviceAddress", async (req, res, next) => {
-    const deviceAddress = req.params.deviceAddress
-    try {
-        const response = await sql.query("select * from recycleproofs where deviceaddress = $1", [deviceAddress])
-        console.log(response.rows)
-        res.send({
-            response: n
-        })
+  try {
+    const deviceAddress = req.params.deviceAddress;
 
-    } catch (e) {
-        console.log(e)
-        next()
+    if (!ethers.utils.isAddress(deviceAddress)) {
+      throw new BadRequest("Wrong address");
     }
-})
 
-module.exports = app
+    const recycleproofs = (
+      await sql.query("select * from recycleproofs where deviceaddress = $1", [
+        deviceAddress,
+      ])
+    ).rows;
+    const functionproofs = (
+      await sql.query("select * from functionproofs where deviceaddress = $1", [
+        deviceAddress,
+      ])
+    ).rows;
+    const transferproofs = (
+      await sql.query("select * from transferproofs where deviceaddress = $1", [
+        deviceAddress,
+      ])
+    ).rows;
+    const datawipeproofs = (
+      await sql.query("select * from datawipeproofs where deviceaddress = $1", [
+        deviceAddress,
+      ])
+    ).rows;
+    const reuseproofs = (
+      await sql.query("select * from reuseproofs where deviceaddress = $1", [
+        deviceAddress,
+      ])
+    ).rows;
+
+    const noData =
+      recycleproofs.length === 0 &&
+      functionproofs.length === 0 &&
+      transferproofs.length === 0 &&
+      datawipeproofs.length === 0 &&
+      reuseproofs.length === 0;
+
+    if (noData) throw new NotFound("Data not found for this device address")
+
+    return res.json({
+      status: "success",
+      data: {
+        proofs: {
+          recycleproofs,
+          functionproofs,
+          transferproofs,
+          datawipeproofs,
+          reuseproofs,
+        },
+      },
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = app;
