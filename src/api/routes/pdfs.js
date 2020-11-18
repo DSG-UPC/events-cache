ae473c41c1cfdb0faf26afabdea479c3a18357d5
Nil Quera <nlquera97@gmail.com>
2020-11-18 16:38:10 +0100

    Change pdf style and add validation link

--- src/api/routes/pdfs.js
+++ src/api/routes/pdfs.js
@@ -3,11 +3,9 @@ const ethers = require("ethers")
 const pdf = require("html-pdf")
 const ejs = require("ejs")
 const path = require("path")
-const signer = require("node-signpdf")
-const fs = require("fs")
-
 const { BadRequest, NotFound } = require("../utils/errors")
 const { getDeviceProofs } = require("../utils/getDeviceProofs")
+require("dotenv").config()
 
 const app = express()
 
@@ -23,49 +21,18 @@ app.get("/devices/:deviceAddress", async (req, res, next) => {
 
     if (noData) throw new NotFound("Data not found for this device address")
 
-    // const ejspdf = await ejs.renderFile(
-    //   path.join(__dirname, "../pdf/template.ejs"),
-    //   {
-    //     deviceAddress,
-    //     proofs,
-    //   }
-    // )
-
-    // const options = {
-    //   height: "11.25in",
-    //   width: "8.5in",
-    //   border: {
-    //     top: "5mm",
-    //     right: "10mm",
-    //     bottom: "5mm",
-    //     left: "10mm",
-    //   },
-    //   header: {
-    //     height: "45mm",
-    //   },
-    //   footer: {
-    //     height: "20mm",
-    //   },
-    // }
-
-    // pdf.create(ejspdf, options).toBuffer((err, buffer) => {
-    //   if (err) console.log(err)
-    //   res.writeHead(200, {
-    //     "Content-Type": "application/pdf",
-    //     "Content-Length": buffer.length,
-    //   })
-    //   res.end(buffer)
-    // })
-
     ejs.renderFile(
-      path.join(__dirname, "../pdf/template.ejs"),
-      { deviceAddress, proofs },
-      (err, ejsFile) => {
-        if (err) throw new Error("error while rendering the ejs template")
+      path.join(__dirname, "../pdf/devicetemplate.ejs"),
+      {
+        deviceAddress,
+        proofs,
+        validateUrl: `${process.env.FRONTENDURL}/?device=${deviceAddress}`,
+        date: new Date().toGMTString(),
+      },
+      (err, html) => {
+        if (err) throw new Error("Error while rendering the ejs template")
         else {
           const options = {
-            height: "11.25in",
-            width: "8.5in",
             border: {
               top: "5mm",
               right: "10mm",
@@ -73,30 +40,16 @@ app.get("/devices/:deviceAddress", async (req, res, next) => {
               left: "10mm",
             },
             header: {
-              height: "45mm",
+              height: "30mm",
             },
             footer: {
-              height: "20mm",
+              height: "25mm",
             },
           }
 
-          pdf.create(ejsFile, options).toBuffer((err, buffer) => {
-            if (err) throw new Error("error while creating the pdf")
+          pdf.create(html, options).toBuffer((err, buffer) => {
+            if (err) throw new Error("Error while creating the pdf")
             else {
-              // const p12Buffer = fs.readFileSync(
-              //   path.join(__dirname, "../pdf/keyStore.p12")
-              // )
-              // const signPdf = new signer.SignPdf()
-              // console.log(signPdf)
-
-              // signPdf.sign(buffer, p12Buffer)
-              // const signature = signPdf.lastSignature
-              // console.log(signature)
-
-              // res.writeHead(200, {
-              //   "Content-Type": "application/pdf",
-              //   "Content-Length": buffer.length,
-              // })
               res.set({
                 "Content-Type": "application/pdf",
                 "Content-Length": buffer.length,
